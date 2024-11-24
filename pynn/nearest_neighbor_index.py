cbf2708ff9b6e2c918da0b4a4db76c1a42a75b91
Harris Lussenhop <hlussenhop@gmail.com>
2024-11-24 00:15:33 -0800

    comments and unit tests

--- pynn/nearest_neighbor_index.py
+++ pynn/nearest_neighbor_index.py
@@ -1,6 +1,6 @@
-from collections import defaultdict
 import math
 import random
+from typing import Dict, List, Optional
 
 
 class NearestNeighborIndex:
@@ -12,14 +12,19 @@ class NearestNeighborIndex:
     points every time.
     """
 
-    def __init__(self, points, grid_size=20):
+    def __init__(self, points: List[(tuple)], grid_size: Optional[int]=20):
         """
-        takes an array of 2d tuples as input points to be indexed.
+        Takes an array of 2d tuples as input points to be indexed. Dynamically generates a 
+        grid index based off of those points and the given grid_size parameter.
+        If grid_size == 0 default to 20.
         """
+        if not isinstance(grid_size, int) or grid_size <= 0:
+            raise ValueError("grid_size must be a positive integer.")
+
         self.points = points
         self.grid_size = grid_size
-        self.bounding_square = self.construct_bounding_square(points)
-        self.cell_width, self.cell_height = self.get_cell_width()
+        self.bounding_square = self.get_bounding_box(points)
+        self.cell_width, self.cell_height = self.get_cell_dimensions()
         self.grid = self.construct_grid_index(points)
 
 
@@ -43,11 +48,24 @@ class NearestNeighborIndex:
 
         return min_point
     
-    def construct_grid_index(self, points):
+    def construct_grid_index(self, points: List[tuple]) -> Dict[tuple, List[tuple]]:
+        """
+        This method creates a spatial index by mapping 2D points to grid cells
+        based on their coordinates and the grid's cell dimensions. Each cell is
+        represented as a key in a dictionary, and the associated value is a list
+        of points that fall within that cell.
+
+        Args:
+            points (List[tuple]):A list of tuples, where each tuple represents 
+                              a point's (x, y) coordinates.
+
+        Returns:
+            (tuple): _description_
+        """
         grid = {}
 
         for point in points:
-            cell_coordinates = self.get_point_square(point)
+            cell_coordinates = self.get_point_cell(point)
             if cell_coordinates in grid:
                 grid[cell_coordinates].append(point)
             else:
@@ -55,7 +73,13 @@ class NearestNeighborIndex:
 
         return grid
 
-    def get_cell_width(self):
+    def get_cell_dimensions(self) -> tuple:
+        """
+        Calculates the dimensions of each grid cell.
+
+        Returns:
+            (tuple): A tuple containing the width and height of each cell.
+        """
         x_min, y_min, x_max, y_max = self.bounding_square
         shift_x = -x_min if x_min < 0 else 0
         shift_y = -y_min if y_min < 0 else 0
@@ -69,31 +93,61 @@ class NearestNeighborIndex:
         cell_height = (y_max - y_min) / self.grid_size
         return cell_width, cell_height
 
-    def get_point_square(self, point):
+    def get_point_cell(self, point: List[tuple]) -> tuple:
+        """
+        Determines the grid cell coordinates for a given point.
+
+        Args:
+            point (List[tuple]): _description_
+
+        Returns:
+            (tuple): cell coordinates.
+        """
         x, y = point
-        
-        cell_x = int(x // self.cell_width)
-        cell_y = int(y // self.cell_height)
-    
+
+        cell_x = math.floor(x / self.cell_width)
+        cell_y = math.floor(y / self.cell_height)
+
         return (cell_x, cell_y)
 
-    def construct_bounding_square(self, points):
+    def get_bounding_box(self, points: List[tuple]) -> tuple:
+        """
+        Constructs a bounding box for a given set of 2D points.
+
+        Args:
+            points (List[tuple]): A list of tuples, where each tuple represents a 
+                              point's (x, y) coordinates.
+
+        Raises:
+            ValueError: If no points are given return a valueError
+
+        Returns:
+            (tuple): tuple containing coordinates of the bounfing box
+        """
         if not points:
             raise ValueError("The list of points is empty.")
-        
+
         x_min = min(point[0] for point in points)
-        x_max = max(point[0] for point in points)
-        y_min = min(point[1] for point in points)
-        y_max = max(point[1] for point in points)
-
-        side_length = max(x_max - x_min, y_max - y_min)
-
-        x_max = x_min + side_length
-        y_max = y_min + side_length
-
+        x_min = min(p[0] for p in points)
+        x_max = max(p[0] for p in points)
+        y_min = min(p[1] for p in points)
+        y_max = max(p[1] for p in points)
         return x_min, y_min, x_max, y_max
 
-    def cell_distance_to_point(self, cell_coords, query_point):
+    def cell_distance_to_point(self, cell_coords: tuple, query_point: tuple) -> int:
+        """    
+        Calculates the distance from a grid cell to a given query point. 
+        If the query point is within or on the bouindaries of the cell, 
+        the distance is 0, otherwise the distance is calculated as:
+        sqrt{(closest_x_in_cell - query_x)^2 + (closest_y_in_cell} - query_y)^2
+        
+        Args:
+            cell_coords (tuple): _description_
+            query_point (tuple): _description_
+
+        Returns:
+            (int): distance from a grid cell to a given query point.
+        """
         q_x, q_y = query_point
         i, j = cell_coords
 
@@ -101,16 +155,31 @@ class NearestNeighborIndex:
         cell_max_x = (i + 1) * self.cell_width
         cell_min_y = j * self.cell_height
         cell_max_y = (j + 1) * self.cell_height
-        
+
         if cell_min_x <= q_x <= cell_max_x and cell_min_y <= q_y <= cell_max_y:
             return 0
-        
+
         closest_x = max(cell_min_x, min(q_x, cell_max_x))
         closest_y = max(cell_min_y, min(q_y, cell_max_y))
-        
+
         return math.sqrt((closest_x - q_x) ** 2 + (closest_y - q_y) ** 2)
 
-    def process_grid_by_distance(self, query_point):
+    def find_nearest(self, query_point: tuple) -> tuple:
+        """
+        Finds the nearest point in the grid to a given query point.
+
+        This method searches the grid to find the closest point (in Euclidean
+        distance) to the specified query point. It iterates over grid cells,
+        prioritizing those closest to the query point based on the distance
+        from the cell to the query point.
+        Args:
+            query_point (tuple): A tuple representing the coordinates of the query point 
+            in 2D space.
+
+        Returns:
+            (tuple):  A tuple representing the coordinates of the nearest neighbor to the 
+            query point.
+        """
         distance_map = {}
         for cell in self.grid:
             distance = self.cell_distance_to_point(cell, query_point)
@@ -137,21 +206,14 @@ class NearestNeighborIndex:
 
         return min_point
 
-    def find_nearest(self, query_point):
-        """
-        TODO comment me.
-        """
-        min_point = self.process_grid_by_distance(query_point)
-        return min_point
-
 # Used for debugging
 if __name__ == "__main__":
 
     def rand_point():
-        return (random.uniform(-1000, 1000), random.uniform(-1000, 1000))
+        return (random.randint(-5, 5), random.randint(-5, 5))
     
     actual = []
-    index_points = [rand_point() for _ in range(1000)]
+    index_points = [rand_point() for _ in range(20)]
     query_points = [rand_point() for _ in range(1)]
 
     uut = NearestNeighborIndex(index_points)
--- tests/test_nearest_neighbor_index.py
+++ tests/test_nearest_neighbor_index.py
@@ -1,11 +1,90 @@
 """nn_search_test"""
 
+import math
 import random
 import time
 import unittest
+import tests.mock_data as mock
 
 from pynn import NearestNeighborIndex
+from tests.mock_data import BASIC_GRID, BASIC_POINTS
 
+class constructGridIndexTest(unittest.TestCase):
+    def test_basic_grid(self):
+        uut = NearestNeighborIndex(BASIC_POINTS, 1)
+
+        grid = uut.construct_grid_index(BASIC_POINTS)
+        self.assertEqual(grid, BASIC_GRID)
+
+    def test_grid_with_negative_points(self):
+        uut = NearestNeighborIndex(mock.NEGATIVE_POINTS, 10)
+
+        grid = uut.construct_grid_index(mock.NEGATIVE_POINTS)
+        self.assertEqual(grid, mock.NEGATIVE_GRID) 
+
+    def test_grid_with_floating_points(self):
+        uut = NearestNeighborIndex(mock.FLOATING_POINTS, 20)
+
+        grid = uut.construct_grid_index(mock.FLOATING_POINTS)
+        self.assertEqual(grid, mock.FLOATING_POINTS_GRID) 
+
+class cellFunctionsTest(unittest.TestCase):
+    def test_basic_cell_diimensions(self):
+        uut = NearestNeighborIndex(BASIC_POINTS, 1)
+
+        cell_dimensions = uut.get_cell_dimensions()
+        self.assertEqual(cell_dimensions, (1,1))
+
+    def test_bigger_cell_dimensions(self):
+        uut = NearestNeighborIndex(mock.FLOATING_POINTS, 20)
+
+        cell_dimensions = uut.get_cell_dimensions()
+        self.assertEqual(cell_dimensions, (10.0,10.0))
+
+    def test_basic_point_cell(self):
+        uut = NearestNeighborIndex(BASIC_POINTS, 1)
+
+        point_assignedd_cell = uut.get_point_cell((1,1))
+        self.assertEqual(point_assignedd_cell, (1,1))
+
+    def test_floating_point_cell(self):
+        uut = NearestNeighborIndex(mock.FLOATING_POINTS, 20)
+
+        point_assignedd_cell = uut.get_point_cell((55.0,-33.00))
+        self.assertEqual(point_assignedd_cell, (5, -4))
+
+class constructBoundingBoxTest(unittest.TestCase):
+    def test_basic_bounding_box(self):
+        uut = NearestNeighborIndex(mock.BASIC_GRID, 1)
+
+        box = uut.construct_bounding_box(mock.BASIC_GRID)
+        self.assertEqual(box, (0,0,1,1))
+
+    def test_floating_points_bounding_box(self):
+        uut = NearestNeighborIndex(mock.BASIC_GRID, 1)
+
+        box = uut.construct_bounding_box(mock.FLOATING_POINTS_GRID)
+        self.assertEqual(box, (-10,-10,10,10))
+
+class distanceToPointTest(unittest.TestCase):
+    def setUp(self):
+        self.grid = NearestNeighborIndex(mock.NEGATIVE_POINTS, 10)
+
+
+    def test_point_inside_cell(self):
+        self.assertEqual(self.grid.cell_distance_to_point((-5, -5), (-5, -5)), 0)
+
+    def test_point_on_corner(self):
+        self.assertEqual(self.grid.cell_distance_to_point((0, 0), (1, 1)), 0)
+
+    def test_point_on_boundary(self):
+        self.assertEqual(self.grid.cell_distance_to_point((0, 0), (0, 1)), 0)
+
+    def test_point_outside_cell(self):
+        self.assertAlmostEqual(self.grid.cell_distance_to_point((0, 0), (8, 9)), 10.6301458)
+
+    def test_negative_coordinates(self):
+        self.assertEqual(self.grid.cell_distance_to_point((0, 0), (2,2)), math.sqrt(2))
 
 class NearestNeighborIndexTest(unittest.TestCase):
     def test_basic(self):
@@ -29,6 +108,11 @@ class NearestNeighborIndexTest(unittest.TestCase):
         self.assertEqual((-1000, 20), uut.find_nearest((-2000, 0)))
         self.assertEqual((42, 3.14159), uut.find_nearest((40, 3)))
 
+    def test_raises_value_error(self):
+        with self.assertRaises(ValueError) as context:
+            uut = NearestNeighborIndex(mock.BASIC_POINTS, -9)
+            self.assertEqual(str(context.exception), "grid_size must be a positive integer.")
+
     def test_benchmark(self):
         """
         test_benchmark tests a bunch of values using the slow and fast version of the index
@@ -38,7 +122,7 @@ class NearestNeighborIndexTest(unittest.TestCase):
         def rand_point():
             return (random.uniform(-1000, 1000), random.uniform(-1000, 1000))
 
-        index_points = [rand_point() for _ in range(50000000)]
+        index_points = [rand_point() for _ in range(50000)]
         query_points = [rand_point() for _ in range(1)]
 
         expected = []
